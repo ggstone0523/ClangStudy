47c492c02fa8b3056640162c8fc04c1fca8a2bca
ggstone0523 <tanks2438@outlook.kr>
2023-08-07 06:03:20 +0000

    Study: solve 1-20 homework
    Signed-off-by: ggstone0523 <tanks2438@outlook.kr>

---
+++ chap1/1-20/detab.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define N 10 /* location of tab stop */
+
+int printDetab(void);
+
+int main()
+{
+	while(printDetab() > 0);
+
+	return 0;
+}
+
+int printDetab(void)
+{
+	char c;
+	int count, i;
+
+	count = 0;
+	while((c = getchar()) != '\n' && c != EOF){
+		if(c == '\t'){
+			for(i=0; i < N - (count % N); ++i){
+				putchar(' ');
+			}
+			count = 0;
+		} else {
+			putchar(c);
+			++count;
+		}
+	}
+	if(c == '\n')
+		putchar(c);
+
+	return count;
+}
