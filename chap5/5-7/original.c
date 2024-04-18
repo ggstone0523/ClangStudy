6eeb51b9daa7c0cf94cbc55a2103312f3e3edf13
Choi Yusun <tanks2438@outlook.kr>
2024-04-18 02:34:52 +0000

    Study: solve 5-7 homework
    Signed-off-by: Choi Yusun <tanks2438@outlook.kr>

---
+++ chap5/5-7/original.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define MAXLINES 5000
+#define MAXLEN 1000
+
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int maxlines);
+void writelines(char *lineptr[], int nlines);
+
+void qSort(char *v[], int left, int right);
+void swap(char *v[], int i, int j);
+
+int getLine(char s[], int lim);
+
+int getLine(char s[], int lim) {
+	int c, i;
+
+	for(i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if(c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+int readlines(char *lineptr[], int maxlines) {
+	int len, nlines;
+	char *p, line[MAXLEN];
+
+	nlines = 0;
+	while((len = getLine(line, MAXLEN)) > 0) {
+		if(nlines >= maxlines || (p = malloc((sizeof(char) * len))) == NULL)
+			return -1;
+		else {
+			line[len-1] = '\0';
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+		}
+	}
+
+	return nlines;
+}
+
+void writelines(char *lineptr[], int nlines) {
+	while(nlines-- > 0)
+		printf("%s\n", *lineptr++);
+}
+
+void qSort(char *v[], int left, int right) {
+	int i, last;
+	
+	if(left >= right)
+		return;
+	swap(v, left, (left+right)/2);
+	last = left;
+	for(i = left+1; i <= right; i++)
+		if(strcmp(v[i], v[left]) < 0)
+			swap(v, ++last, i);
+	swap(v, left, last);
+	qSort(v, left, last-1);
+	qSort(v, last+1, right);
+}
+
+void swap(char *v[], int i, int j) {
+	char *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
+int main() {
+	int nlines;
+	struct timeval stop, start;
+	
+	gettimeofday(&start, NULL);
+	if((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+		qSort(lineptr, 0, nlines-1);
+		writelines(lineptr, nlines);
+		gettimeofday(&stop, NULL);
+		printf("time 2:%lu\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);	
+		return 0;
+	} else {
+		printf("error: input too big to sort\n");
+		return 1;
+	}
+}
+
